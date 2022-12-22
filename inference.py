d58cd48a4a353f52205a3aa7db9ee9565f5349c1
Ivan Moshkov <i-vainn@zomb-research-cv-1.zombie.yandex.net>
2022-12-22 15:07:56 +0300

    now you can test it

---
+++ inference.py
@@ -0,0 +1,35 @@
+import os
+import wget
+import torch
+import torchaudio
+
+from source.generator_model import Generator
+from source.dataset import MelSpectrogram
+from source.configs import MelSpectrogramConfig
+
+CHECKPOINT_URL = 'https://api.wandb.ai/files/i_vainn/hifigan/anzmldlc/checkpoints-V1/checkpoint_6ep.pth?_gl=1*a2v382*_ga*MTc4NzY3MDAxNy4xNjcwMjQwMjc2*_ga_JH1SJHJQXJ*MTY3MTcwNDY5Ny40NS4xLjE2NzE3MDc1NDYuNTMuMC4w'
+CHECKPOINT_PATH = 'model_best.pth'
+TEST_PATH = 'data/test/wavs'
+OUTPUT_PATH = 'data/test/generated_wavs'
+DEVICE = 'cpu'
+
+if __name__ == '__main__':
+    wget.download(CHECKPOINT_URL, CHECKPOINT_PATH)
+    generator = Generator()
+    generator.to(DEVICE)
+    generator.load_state_dict(torch.load(CHECKPOINT_PATH)['generator'])
+    melspec = MelSpectrogram(MelSpectrogramConfig())
+    generator.eval()
+
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+
+    print('\nGenerating...')
+
+    with torch.inference_mode():
+        for file in os.listdir(TEST_PATH):
+            original_wav, sr = torchaudio.load(os.path.join(TEST_PATH, file))
+            original_mel = melspec(original_wav).to(DEVICE)
+            generated_wav = generator(original_mel)
+            torchaudio.save(os.path.join(OUTPUT_PATH, file), generated_wav, sr)
+    
+    print('Done!')
---
+++ setup_data.sh
@@ -0,0 +1,4 @@
+wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+tar -xvf LJSpeech-1.1.tar.bz2
+mv LJSpeech-1.1 data/LJSpeech-1.1
+rm LJSpeech-1.1.tar.bz2
